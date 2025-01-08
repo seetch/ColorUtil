6ffb51d87fbb03c59f0ecc88b45e6f7dbc0afae4
seetch <seetch@inbox.ru>
2025-01-08 20:59:59 +0300

    Improve ColorUtil class for better usability and readability

--- src/main/java/su/daycube/ColorUtil.java
+++ src/main/java/su/daycube/ColorUtil.java
@@ -2,54 +2,80 @@ package su.daycube;
 
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ColorUtil {
 
+    /**
+     * Applies color codes to the given value.
+     *
+     * @param value the value to colorize
+     * @param <T>   the type of the value
+     * @return the colorized value
+     */
     @Nullable
-    public static Object colorize(@Nullable Object value) {
+    public static <T> T colorize(@Nullable T value) {
         if (value != null) {
             if (value instanceof String) {
-                return formatColorCodes((String) value);
+                return (T) formatColorCodes((String) value);
             } else if (value instanceof List) {
                 List<Object> list = (List<Object>) value;
                 if (!list.isEmpty() && list.get(0) instanceof String) {
                     list.replaceAll(o -> formatColorCodes((String) o));
-                    return list;
+                    return (T) list;
                 }
             }
         }
         return value;
     }
 
+    /**
+     * Removes color codes from the given value.
+     *
+     * @param value the value to uncolorize
+     * @param <T>   the type of the value
+     * @return the uncolorized value
+     */
     @Nullable
-    public static Object uncolorize(@Nullable Object value) {
+    public static <T> T uncolorize(@Nullable T value) {
         if (value != null) {
             if (value instanceof String) {
-                return ((String) value).replace(ChatColor.COLOR_CHAR, '&');
+                return (T) ((String) value).replace(ChatColor.COLOR_CHAR, '&');
             } else if (value instanceof List) {
                 List<Object> list = (List<Object>) value;
                 if (!list.isEmpty() && list.get(0) instanceof String) {
                     list.replaceAll(o -> ((String) o).replace(ChatColor.COLOR_CHAR, '&'));
-                    return list;
+                    return (T) list;
                 }
             }
         }
         return value;
     }
 
+    /**
+     * Formats the color codes in the given string.
+     *
+     * @param str the string to format
+     * @return the formatted string
+     */
     @Contract("null -> null")
-    public static String formatColorCodes(@Nullable String str) {
+    private static String formatColorCodes(@Nullable String str) {
         if (str == null) return null;
         return ChatColor.translateAlternateColorCodes('&', str);
     }
 
+    /**
+     * Formats the color codes in the given list of strings.
+     *
+     * @param listStr the list of strings to format
+     * @return the formatted list of strings
+     */
     @Contract("null -> null")
-    public static List<String> formatColorCodes(@Nullable List<String> listStr) {
+    private static List<String> formatColorCodes(@Nullable List<String> listStr) {
         if (listStr == null) return null;
         return listStr.stream().filter(Objects::nonNull)
                 .map(ColorUtil::formatColorCodes).collect(Collectors.toList());
